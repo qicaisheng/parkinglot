afe743a82a27cb4d2f33ecd3785d42e8ae0aa32e
qicaisheng <qicaisheng@qq.com>
2019-05-07 09:05:45 +0800

    car can not pick two times

--- src/test/java/com/qicaisheng/parkinglot/ParkingLot.java
+++ src/test/java/com/qicaisheng/parkinglot/ParkingLot.java
@@ -1,11 +1,17 @@
 package com.qicaisheng.parkinglot;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class ParkingLot {
+    
+    private List<Car> parkedCar = new ArrayList<>();
+    
     public void park(Car car) {
-        
+        parkedCar.add(car);
     }
 
     public boolean pick(Car car) {
-        return true;
+        return parkedCar.remove(car);
     }
 }
--- src/test/java/com/qicaisheng/parkinglot/ParkingLotTest.java
+++ src/test/java/com/qicaisheng/parkinglot/ParkingLotTest.java
@@ -15,4 +15,17 @@ public class ParkingLotTest {
         boolean canBePick = parkingLot.pick(car);
         Assert.assertTrue(canBePick);
     }
+
+    @Test
+    public void should_not_be_able_to_pick_up_again_when_the_card_pick_up_from_parking_lot() {
+        Car car = new Car();
+        ParkingLot parkingLot = new ParkingLot();
+        parkingLot.park(car);
+
+        parkingLot.pick(car);
+        
+        boolean canBePick = parkingLot.pick(car);
+        Assert.assertFalse(canBePick);
+    }
+
 }

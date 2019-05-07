61649494a02bf93f7eaf50dda16b4e9ee6710fc9
qicaisheng <qicaisheng@qq.com>
2019-05-07 12:48:02 +0800

    add isFull method to parking lot

--- src/test/java/com/qicaisheng/parkinglot/ParkingLot.java
+++ src/test/java/com/qicaisheng/parkinglot/ParkingLot.java
@@ -5,13 +5,23 @@ import java.util.List;
 
 public class ParkingLot {
     
-    private List<Car> parkedCar = new ArrayList<>();
+    private int capacity;
     
+    private List<Car> parkedCar = new ArrayList<>();
+
+    public ParkingLot(int capacity) {
+        this.capacity = capacity;
+    }
+
     public void park(Car car) {
         parkedCar.add(car);
     }
 
     public boolean pick(Car car) {
         return parkedCar.remove(car);
+    }
+
+    public boolean isFull() {
+        return parkedCar.size() >= capacity;
     }
 }
--- src/test/java/com/qicaisheng/parkinglot/ParkingLotTest.java
+++ src/test/java/com/qicaisheng/parkinglot/ParkingLotTest.java
@@ -8,7 +8,7 @@ public class ParkingLotTest {
     @Test
     public void should_be_able_to_pick_up_when_the_card_park_into_parking_lot() {
         Car car = new Car();
-        ParkingLot parkingLot = new ParkingLot();
+        ParkingLot parkingLot = new ParkingLot(1);
         
         parkingLot.park(car);
         
@@ -19,13 +19,33 @@ public class ParkingLotTest {
     @Test
     public void should_not_be_able_to_pick_up_again_when_the_card_pick_up_from_parking_lot() {
         Car car = new Car();
-        ParkingLot parkingLot = new ParkingLot();
+        ParkingLot parkingLot = new ParkingLot(1);
         parkingLot.park(car);
 
         parkingLot.pick(car);
         
         boolean canBePick = parkingLot.pick(car);
         Assert.assertFalse(canBePick);
+    }
+
+    @Test
+    public void should_be_full_when_the_parked_car_size_meet_parking_lot_max_capacity() {
+        Car car = new Car();
+        ParkingLot parkingLot = new ParkingLot(1);
+        
+        parkingLot.park(car);
+
+        Assert.assertTrue(parkingLot.isFull());
+    }
+
+    @Test
+    public void should_not_be_full_when_the_parked_car_size_not_meet_parking_lot_max_capacity() {
+        Car car = new Car();
+        ParkingLot parkingLot = new ParkingLot(2);
+
+        parkingLot.park(car);
+
+        Assert.assertFalse(parkingLot.isFull());
     }
 
 }

5b3b2ebb1e2bab6b0096bb9c0d6a8bd14b1517b8
qicaisheng <qicaisheng@qq.com>
2019-05-07 20:54:47 +0800

    make smart parking boy extends parking boy

---
+++ src/main/java/com/qicaisheng/parkinglot/SmartParkingBoy.java
@@ -0,0 +1,9 @@
+package com.qicaisheng.parkinglot;
+
+import java.util.List;
+
+public class SmartParkingBoy extends ParkingBoy{
+    public SmartParkingBoy(List<ParkingLot> parkingLots) {
+        super(parkingLots);
+    }
+}

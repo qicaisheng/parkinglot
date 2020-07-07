fe9952e638abf0d7d158720ab69c41f0cb742731
qicaisheng <qicaisheng@qq.com>
2020-07-07 15:59:07 +0800

    move getSelfReportData to ReportFactory

--- src/main/java/com/qicaisheng/parkinglot/ParkingResource.java
+++ src/main/java/com/qicaisheng/parkinglot/ParkingResource.java
@@ -2,10 +2,6 @@ package com.qicaisheng.parkinglot;
 
 public abstract class ParkingResource {
 
-    public String getSelfReportData(String prefix, ParkingResource parkingResource) {
-        return prefix + parkingResource.getShortName() + " " + parkingResource.getAvailableSpaces() + " " + parkingResource.getCapacity() + "\n";
-    }
-
     public abstract String getShortName();
 
     public abstract int getAvailableSpaces();
--- src/main/java/com/qicaisheng/parkinglot/ReportFactory.java
+++ src/main/java/com/qicaisheng/parkinglot/ReportFactory.java
@@ -5,8 +5,8 @@ import java.util.stream.Collectors;
 public class ReportFactory {
 
     public static String getTextReport(ParkingManager parkingManager) {
-        String reportSelf = parkingManager.getSelfReportData("", parkingManager);
-        String reportSelfManagedParkingLots = parkingManager.getManagedParkingLots().stream().map(parkingLot -> parkingLot.getSelfReportData("\t", parkingLot)).collect(Collectors.joining());
+        String reportSelf = getSelfReportData("", parkingManager);
+        String reportSelfManagedParkingLots = parkingManager.getManagedParkingLots().stream().map(parkingLot -> getSelfReportData("\t", parkingLot)).collect(Collectors.joining());
         String reportSelfManagedParkingAgents = parkingManager.getManagedParkingBoys().stream().map(parkingAgent -> "\t" + getTextReport(parkingAgent)).collect(Collectors.joining());
 
         return reportSelf + reportSelfManagedParkingLots + reportSelfManagedParkingAgents;
@@ -14,23 +14,26 @@ public class ReportFactory {
 
 
     public static String getMarkdownReport(ParkingManager parkingManager) {
-        String reportSelf = parkingManager.getSelfReportData("# ", parkingManager);
-        String reportSelfManagedParkingLots = parkingManager.getManagedParkingLots().stream().map(parkingLot -> parkingLot.getSelfReportData("## ", parkingLot)).collect(Collectors.joining());
+        String reportSelf = getSelfReportData("# ", parkingManager);
+        String reportSelfManagedParkingLots = parkingManager.getManagedParkingLots().stream().map(parkingLot -> getSelfReportData("## ", parkingLot)).collect(Collectors.joining());
         String reportSelfManagedParkingAgents = parkingManager.getManagedParkingBoys().stream().map(parkingAgent -> "#" + getMarkdownReport(parkingAgent)).collect(Collectors.joining());
 
         return reportSelf + reportSelfManagedParkingLots + reportSelfManagedParkingAgents;
     }
 
     private static String getTextReport(ParkingAgent parkingAgent) {
-        String reportSelf = parkingAgent.getSelfReportData("", parkingAgent);
-        String reportMangedParkingLots = parkingAgent.getManagedParkingLots().stream().map(parkingLot -> parkingLot.getSelfReportData("\t\t", parkingLot)).collect(Collectors.joining());
+        String reportSelf = getSelfReportData("", parkingAgent);
+        String reportMangedParkingLots = parkingAgent.getManagedParkingLots().stream().map(parkingLot -> getSelfReportData("\t\t", parkingLot)).collect(Collectors.joining());
         return reportSelf + reportMangedParkingLots;
     }
 
     private static String getMarkdownReport(ParkingAgent parkingAgent) {
-        String reportSelf = parkingAgent.getSelfReportData("# ", parkingAgent);
-        String reportMangedParkingLots = parkingAgent.getManagedParkingLots().stream().map(parkingLot -> parkingLot.getSelfReportData("### ", parkingLot)).collect(Collectors.joining());
+        String reportSelf = getSelfReportData("# ", parkingAgent);
+        String reportMangedParkingLots = parkingAgent.getManagedParkingLots().stream().map(parkingLot -> getSelfReportData("### ", parkingLot)).collect(Collectors.joining());
         return reportSelf + reportMangedParkingLots;
     }
 
+    public static String getSelfReportData(String prefix, ParkingResource parkingResource) {
+        return prefix + parkingResource.getShortName() + " " + parkingResource.getAvailableSpaces() + " " + parkingResource.getCapacity() + "\n";
+    }
 }

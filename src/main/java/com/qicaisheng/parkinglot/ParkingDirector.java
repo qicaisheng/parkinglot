ec0316ff511542de0a893c5a92f0e1e9bb543e69
qicaisheng <qicaisheng@qq.com>
2020-07-07 18:17:02 +0800

    use reportFactory

--- src/main/java/com/qicaisheng/parkinglot/ParkingDirector.java
+++ src/main/java/com/qicaisheng/parkinglot/ParkingDirector.java
@@ -10,10 +10,10 @@ public class ParkingDirector {
 
     public String report(ReportFactory reportFactory) {
         if (reportFactory instanceof TextReportFactory) {
-            return new TextReportFactory().report(managedParkingManager);
+            return reportFactory.report(managedParkingManager);
         }
         if (reportFactory instanceof MarkdownReportFactory) {
-            return new MarkdownReportFactory().report(managedParkingManager);
+            return reportFactory.report(managedParkingManager);
         }
         return null;
     }

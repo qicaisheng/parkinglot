bbd5a79753f6c8744444e9432bac42ed9d48ffe8
qicaisheng <qicaisheng@qq.com>
2020-07-07 18:43:17 +0800

    modify visit parkingAgent method

--- src/main/java/com/qicaisheng/parkinglot/MarkdownReportVisitor.java
+++ src/main/java/com/qicaisheng/parkinglot/MarkdownReportVisitor.java
@@ -8,16 +8,16 @@ public class MarkdownReportVisitor extends ReportVisitor {
     public String visit(ParkingManager parkingManager) {
         String reportSelf = visit("# ", parkingManager);
         String reportSelfManagedParkingLots = parkingManager.getManagedParkingLots().stream().map(parkingLot -> visit("## ", parkingLot)).collect(Collectors.joining());
-        String reportSelfManagedParkingAgents = parkingManager.getManagedParkingBoys().stream().map(parkingAgent -> "#" + visit(parkingAgent)).collect(Collectors.joining());
+        String reportSelfManagedParkingAgents = parkingManager.getManagedParkingBoys().stream().map(this::visit).collect(Collectors.joining());
 
         return reportSelf + reportSelfManagedParkingLots + reportSelfManagedParkingAgents;
     }
     
     @Override
     public String visit(ParkingAgent parkingAgent) {
-        String reportSelf = visit("# ", parkingAgent);
+        String reportSelf = visit("## ", parkingAgent);
         String reportMangedParkingLots = parkingAgent.getManagedParkingLots().stream().map(parkingLot -> visit("### ", parkingLot)).collect(Collectors.joining());
         return reportSelf + reportMangedParkingLots;
     }
-
+    
 }
--- src/main/java/com/qicaisheng/parkinglot/TextReportVisitor.java
+++ src/main/java/com/qicaisheng/parkinglot/TextReportVisitor.java
@@ -8,14 +8,14 @@ public class TextReportVisitor extends ReportVisitor {
     public String visit(ParkingManager parkingManager) {
         String reportSelf = visit("", parkingManager);
         String reportSelfManagedParkingLots = parkingManager.getManagedParkingLots().stream().map(parkingLot -> visit("\t", parkingLot)).collect(Collectors.joining());
-        String reportSelfManagedParkingAgents = parkingManager.getManagedParkingBoys().stream().map(parkingAgent -> "\t" + visit(parkingAgent)).collect(Collectors.joining());
+        String reportSelfManagedParkingAgents = parkingManager.getManagedParkingBoys().stream().map(this::visit).collect(Collectors.joining());
 
         return reportSelf + reportSelfManagedParkingLots + reportSelfManagedParkingAgents;
     }
     
     @Override
     public String visit(ParkingAgent parkingAgent) {
-        String reportSelf = visit("", parkingAgent);
+        String reportSelf = visit("\t", parkingAgent);
         String reportMangedParkingLots = parkingAgent.getManagedParkingLots().stream().map(parkingLot -> visit("\t\t", parkingLot)).collect(Collectors.joining());
         return reportSelf + reportMangedParkingLots;
     }

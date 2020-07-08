517771eca544ef87fa6c301464dd5ff0af71b5a6
qicaisheng <qicaisheng@qq.com>
2020-07-08 11:35:37 +0800

    move visit parkingManager method to ReportVisitor

--- src/main/java/com/qicaisheng/parkinglot/ReportVisitor.java
+++ src/main/java/com/qicaisheng/parkinglot/ReportVisitor.java
@@ -1,11 +1,19 @@
 package com.qicaisheng.parkinglot;
 
+import java.util.stream.Collectors;
+
 public abstract class ReportVisitor {
     public String visit(String prefix, ParkingResource parkingResource) {
         return prefix + parkingResource.getShortName() + " " + parkingResource.getAvailableSpaces() + " " + parkingResource.getCapacity() + "\n";
     }
 
-    public abstract String visit(ParkingManager parkingManager);
+    public String visit(ParkingManager parkingManager) {
+        String reportSelf = visitSelf(parkingManager);
+        String reportSelfManagedParkingLots = parkingManager.getManagedParkingLots().stream().map(parkingLot -> visitSelfInParkingManager(parkingLot)).collect(Collectors.joining());
+        String reportSelfManagedParkingAgents = parkingManager.getManagedParkingBoys().stream().map(this::visit).collect(Collectors.joining());
+
+        return reportSelf + reportSelfManagedParkingLots + reportSelfManagedParkingAgents;
+    }
 
     public abstract String visitSelfInParkingManager(ParkingLot parkingLot);
 
--- src/main/java/com/qicaisheng/parkinglot/TextReportVisitor.java
+++ src/main/java/com/qicaisheng/parkinglot/TextReportVisitor.java
@@ -3,16 +3,7 @@ package com.qicaisheng.parkinglot;
 import java.util.stream.Collectors;
 
 public class TextReportVisitor extends ReportVisitor {
-
-    @Override
-    public String visit(ParkingManager parkingManager) {
-        String reportSelf = visitSelf(parkingManager);
-        String reportSelfManagedParkingLots = parkingManager.getManagedParkingLots().stream().map(parkingLot -> visitSelfInParkingManager(parkingLot)).collect(Collectors.joining());
-        String reportSelfManagedParkingAgents = parkingManager.getManagedParkingBoys().stream().map(this::visit).collect(Collectors.joining());
-
-        return reportSelf + reportSelfManagedParkingLots + reportSelfManagedParkingAgents;
-    }
-
+    
     @Override
     public String visitSelfInParkingManager(ParkingLot parkingLot) {
         return visit("\t", parkingLot);

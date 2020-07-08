c60a2b03a662e75b588f62e822da3b2390be2258
qicaisheng <qicaisheng@qq.com>
2020-07-08 11:21:24 +0800

    extract methods in MarkdownReportVisitor

--- src/main/java/com/qicaisheng/parkinglot/MarkdownReportVisitor.java
+++ src/main/java/com/qicaisheng/parkinglot/MarkdownReportVisitor.java
@@ -6,18 +6,30 @@ public class MarkdownReportVisitor extends ReportVisitor {
 
     @Override
     public String visit(ParkingManager parkingManager) {
-        String reportSelf = visit("# ", parkingManager);
-        String reportSelfManagedParkingLots = parkingManager.getManagedParkingLots().stream().map(parkingLot -> visit("## ", parkingLot)).collect(Collectors.joining());
+        String reportSelf = visitSelf(parkingManager);
+        String reportSelfManagedParkingLots = parkingManager.getManagedParkingLots().stream().map(parkingLot -> visitSelfInParkingManager(parkingLot)).collect(Collectors.joining());
         String reportSelfManagedParkingAgents = parkingManager.getManagedParkingBoys().stream().map(this::visit).collect(Collectors.joining());
 
         return reportSelf + reportSelfManagedParkingLots + reportSelfManagedParkingAgents;
     }
-    
+
+    private String visitSelfInParkingManager(ParkingLot parkingLot) {
+        return visit("## ", parkingLot);
+    }
+
+    private String visitSelf(ParkingManager parkingManager) {
+        return visit("# ", parkingManager);
+    }
+
     @Override
     public String visit(ParkingAgent parkingAgent) {
         String reportSelf = visit("## ", parkingAgent);
-        String reportMangedParkingLots = parkingAgent.getManagedParkingLots().stream().map(parkingLot -> visit("### ", parkingLot)).collect(Collectors.joining());
+        String reportMangedParkingLots = parkingAgent.getManagedParkingLots().stream().map(parkingLot -> visitSelfInParkingAgent(parkingLot)).collect(Collectors.joining());
         return reportSelf + reportMangedParkingLots;
     }
-    
+
+    private String visitSelfInParkingAgent(ParkingLot parkingLot) {
+        return visit("### ", parkingLot);
+    }
+
 }

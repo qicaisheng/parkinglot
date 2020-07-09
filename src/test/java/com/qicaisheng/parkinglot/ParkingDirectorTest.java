2d738b274d971ed520de74a1253d15cddcedbcd4
qicaisheng <qicaisheng@qq.com>
2020-07-09 09:38:50 +0800

    should_get_html_report_with_manager_managed_parking_lots_and_parking_boys

--- src/test/java/com/qicaisheng/parkinglot/ParkingDirectorTest.java
+++ src/test/java/com/qicaisheng/parkinglot/ParkingDirectorTest.java
@@ -58,4 +58,23 @@ public class ParkingDirectorTest {
 
         Assert.assertEquals("# M 1 3\n## P 0 1\n## P 1 2\n## B 0 1\n### P 0 1\n", report);
     }
+
+    @Test
+    public void should_get_html_report_with_manager_managed_parking_lots_and_parking_boys() {
+        ParkingLot parkingLot1 = new ParkingLot(1);
+        ParkingLot parkingLot2 = new ParkingLot(2);
+        parkingLot2.park(new Car());
+        ParkingManager parkingManager = new ParkingManager(Arrays.asList(parkingLot1, parkingLot2));
+
+        ParkingAgent parkingAgent = new ParkingBoy(Arrays.asList(parkingLot1));
+        parkingAgent.park(new Car());
+
+        parkingManager.manager(Arrays.asList(parkingAgent));
+        ParkingDirector parkingDirector = new ParkingDirector();
+        parkingDirector.manage(parkingManager);
+
+        String report = parkingDirector.managedParkingManager.accept(new HTMLReportVisitor());
+
+        Assert.assertEquals("<h2>M 1 3</h2><h3>P 0 1</h3><h3>P 1 2</h3><h3>B 0 1</h3><div>P 0 1</div>", report);
+    }
 }
